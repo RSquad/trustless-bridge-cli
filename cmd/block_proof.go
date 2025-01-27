1a518964429fcb18428b1e46362186162d632b22
inyellowbus <inyellowbus@gmail.com>
2025-01-28 01:36:31 +0700

    feat(block): implement block proof generation only for masterchain
    - Add full implementation for block proof for masterchain blocks
    - Update README description to reflect proof generation capabilities

--- cmd/block_proof.go
+++ cmd/block_proof.go
@@ -17,21 +17,104 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/xssnick/tonutils-go/ton"
+	"github.com/xssnick/tonutils-go/tvm/cell"
 )
 
 var blockProofCmd = &cobra.Command{
 	Use:   "proof",
-	Short: "Generate a proof for a block",
+	Short: "Generate a proof for a block (currently works only with blocks from the masterchain)",
 	Run:   runBlockProof,
 }
 
 func init() {
 	blockCmd.AddCommand(blockProofCmd)
+	blockProofCmd.Flags().Uint32("from-seqno", 0, "From block seqno")
+	blockProofCmd.Flags().Uint32("to-seqno", 0, "To block seqno")
+	// blockProofCmd.Flags().Int32("to-workchain", -1, "To workchain")
+	// blockProofCmd.Flags().Int64("to-shard", 0, "To shard")
+	blockProofCmd.Flags().StringP("output-format", "f", "bin", "Output format: bin, hex")
+	blockProofCmd.MarkFlagRequired("from-seqno")
+	blockProofCmd.MarkFlagRequired("to-seqno")
 }
 
 func runBlockProof(cmd *cobra.Command, args []string) {
-	fmt.Println("not implemented")
+	outputFormat, err := cmd.Flags().GetString("output-format")
+	if err != nil {
+		panic(err)
+	}
+	fromSeqno, err := cmd.Flags().GetUint32("from-seqno")
+	if err != nil {
+		panic(err)
+	}
+	toSeqno, err := cmd.Flags().GetUint32("to-seqno")
+	if err != nil {
+		panic(err)
+	}
+	// toShard, err := cmd.Flags().GetInt64("to-shard")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// toWorkchain, err := cmd.Flags().GetInt32("to-workchain")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	toWorkchain := int32(-1)
+	fromWorkchain := int32(-1)
+
+	fromBlockIDExt, err := tonClient.API.LookupBlock(
+		context.Background(),
+		fromWorkchain,
+		0,
+		fromSeqno,
+	)
+	if err != nil {
+		panic(err)
+	}
+	toBlockIDExt, err := tonClient.API.LookupBlock(
+		context.Background(),
+		toWorkchain,
+		0,
+		toSeqno,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	blockProof, err := tonClient.API.GetBlockProof(
+		context.Background(),
+		fromBlockIDExt,
+		toBlockIDExt,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	var result *cell.Cell
+
+	for _, step := range blockProof.Steps {
+		if back, ok := step.(ton.BlockLinkBackward); ok {
+			boc, err := cell.FromBOC(back.Proof)
+			if err != nil {
+				panic(err)
+			}
+			result = boc
+		}
+	}
+
+	switch outputFormat {
+	case "hex":
+		fmt.Printf("%x\n", result.ToBOC())
+
+	case "bin":
+		fallthrough
+	default:
+		os.Stdout.Write(result.ToBOC())
+	}
 }
